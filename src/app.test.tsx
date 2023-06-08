52f8727d6b37066afbfd71656c365ef695b7f8dd
Rakesh Chintha <chintha.rakeshreddy@gmail.com>
2023-06-08 00:18:46 -0500

    Initial commit

---
+++ src/app.test.tsx
@@ -0,0 +1,10 @@
+import { render, fireEvent, screen, waitFor } from '@testing-library/preact';
+import { App } from './app';
+
+describe("App", () => {
+    test("should render", () => {
+        const { container } = render(<App/>);
+        expect(container.textContent).toMatch('Vite + Preact');
+    });
+  });
+  
---
+++ src/sample.test.ts
@@ -0,0 +1,5 @@
+describe("Sample", () => {
+  test("should validate addition", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
